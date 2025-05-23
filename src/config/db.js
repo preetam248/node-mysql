9efe3c09554d22fe367a8e480b915e941c3a5a93
Preetam <preetamdas248@gmail.com>
2025-05-23 17:08:02 +0530

    edit .env

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const mysqlPool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = mysqlPool;
