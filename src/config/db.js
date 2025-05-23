7c942d7e9ff305d2b9ca0d76d830b8797fe325df
Preetam <preetamdas248@gmail.com>
2025-05-23 17:10:36 +0530

    edit .env file

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,9 @@ const mysqlPool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true, // Aiven requires SSL
+  },
 });
 
 module.exports = mysqlPool;
